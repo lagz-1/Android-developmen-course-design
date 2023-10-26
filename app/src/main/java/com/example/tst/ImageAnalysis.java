bde1566521b7afe12328214884d8084e3e7b66f7
lagz-1 <2433006449@qq.com>
2023-10-26 21:35:09 +0800

    SSIM算法

--- app/src/main/java/com/example/tst/ImageAnalysis.java
+++ app/src/main/java/com/example/tst/ImageAnalysis.java
@@ -1,13 +1,18 @@
 package com.example.tst;
 
+import static java.lang.Double.max;
+
 import com.example.tst.R;
+import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,11 +21,19 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class ImageAnalysis extends AppCompatActivity {
 
@@ -31,31 +44,64 @@ public class ImageAnalysis extends AppCompatActivity {
     String str = new String();
 
     Bitmap bitmapAnalyse;
+    String[] comparedPic = new String[]{"white","pic_20_rank1","pic_40_rank2","pic_60_rank3","pic_80_rank4","black"};
+
+    int flag = -1;
+    public Bitmap getDrawable(String name)
+    {
+        ApplicationInfo appInfo = getApplicationInfo();
+        int resID = getResources().getIdentifier(name, "drawable", appInfo.packageName);
+        //解析资源文件夹下，id为resID的图片
+        return BitmapFactory.decodeResource(getResources(),  resID);
+    }
+
+    public Mat getDrawableToMat(Bitmap bitmap)
+    {
+        Mat matOp = new Mat(bitmap.getHeight(), bitmap.getWidth(), CvType.CV_8U);
+        Utils.bitmapToMat(bitmap,matOp);
+        return matOp;
+
+    }
+
+    // 计算结构相似性指数（SSIM）
+    private static double calculateSSIM(Mat image1, Mat image2) {
+        MatOfFloat ssimMat = new MatOfFloat();
+        Imgproc.matchTemplate(image1, image2, ssimMat, Imgproc.CV_COMP_CORREL);
+        Scalar ssimScalar = Core.mean(ssimMat);
+        return ssimScalar.val[0];
+    }
+
+
+
+
+
+
 
     public double Analyse(Bitmap bitmap){
-        Mat matOp = new Mat(bitmap.getHeight(), bitmap.getWidth(), CvType.CV_8UC4);
+        Mat matOp = new Mat(bitmap.getHeight(), bitmap.getWidth(), CvType.CV_8U);
         Utils.bitmapToMat(bitmap, matOp);
-        Utils.bitmapToMat(bitmap, matOp);
-        Imgproc.cvtColor(matOp, matOp, Imgproc.COLOR_BGR2GRAY); // 转换为灰度图	        Imgproc.cvtColor(matOp, matOp, Imgproc.COLOR_BGR2GRAY); // 转换为灰度图
+        Imgproc.cvtColor(matOp, matOp, Imgproc.COLOR_BGR2GRAY); // 转换为灰度图
 
-        int resourceIdWHITE = getResources().getIdentifier("white", "drawable", getPackageName());
-        if (resourceIdWHITE != 0) {
-            // 如果找到资源标识符，则加载图片
-            tv2.setImageResource(resourceIdWHITE);
-        } else {
-            // 如果未找到资源，可以采取适当的处理措施，例如显示默认图片或显示错误消息
+        double maxsimilarity = 0;
+
+
+        for(int i = 0;i < 6;i++){
+            Mat matCompared = getDrawableToMat(getDrawable(comparedPic[i]));
+            Imgproc.cvtColor(matCompared, matCompared, Imgproc.COLOR_BGR2GRAY); // 转换为灰度图
+
+
+            Imgproc.resize(matOp, matOp, matCompared.size());
+            Imgproc.resize(matCompared, matCompared, matOp.size());
+
+            double ssim = calculateSSIM(matOp, matCompared);
+            Log.e("(SSIM)", String.valueOf(ssim));
+
+            if(ssim>=maxsimilarity){
+                flag = i;
+                maxsimilarity = ssim;
+            }
         }
-
-        int resourceIdBLACK = getResources().getIdentifier("black", "drawable", getPackageName());
-        if (resourceIdBLACK != 0) {
-            // 如果找到资源标识符，则加载图片
-            tv2.setImageResource(resourceIdBLACK);
-        } else {
-            // 如果未找到资源，可以采取适当的处理措施，例如显示默认图片或显示错误消息
-        }
-
-
-        return 0.1;
+        return maxsimilarity*flag;
     }
 
 
